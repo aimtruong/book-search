c42a116cfa28dbdcdfe6f6ab06205297b9664f5e
Aimee Truong <aimtruong@ucdavis.edu>
2022-05-21 20:42:27 -0700

    resolvers query added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,15 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if(context.user){
+                const user = await User.findById(context.user._id).populate('Book');
 
+                return user;
+            };
+
+            throw new AuthenticationError('Not logged in');
+        }
     },
 
     Mutation: {
