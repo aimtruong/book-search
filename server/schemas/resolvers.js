d3aae32caaba07a6284b8e971d7bb933681f1e2b
Aimee Truong <aimtruong@ucdavis.edu>
2022-05-21 20:04:45 -0700

    add mutations to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,11 +5,53 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+
     },
 
     Mutation: {
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            
+            if(!user){
+                throw new AuthenticationError('Incorrect credentials');
+            }
 
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async(parent, { books }, context) => {
+            if(context.user){
+                const book = new Book({ books });
+
+                await User.findByIdAndUpdate(context.user._id, { $addToSet: { books: book } });
+
+                return book;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+        removeBook: async(parent, { bookId }) => {
+            const book = await Book.findOne({ bookId: bookId })
+
+            if(context.user){
+                return await User.findByIdAndUpdate(context.user._id, { $pull: { Book: book } });
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
     }
 }
 
